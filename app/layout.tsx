78e21ea2fd553d8ab5b04b7e81f0b255fe5f0686
PM <mireabdu3@gmail.com>
2025-01-12 15:53:28 +0200

    styole

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../pages/App.css';
+import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Error 404: Access Denied | Secure Server',
-  description: 'Error 404: The requested resource could not be found. Access to this page is restricted.',
+  title: 'Error 404: Access Denied',
+  description: 'The requested resource could not be found. Access to this page is restricted.',
 }
 
 export default function RootLayout({
